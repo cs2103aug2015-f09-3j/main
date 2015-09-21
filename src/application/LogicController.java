b9244d4db44bf82336e2e7f09843de813420da5b
YouLiang <youliang87@gmail.com>
2015-09-21 21:00:48 +0800

    First Commit. Skeleton Code

---
+++ src/application/LogicController.java
@@ -0,0 +1,29 @@
+package application;
+/**
+ * This is a singleton class
+ * 
+ * @author youlianglim
+ *
+ */
+public class LogicController {
+	
+	private static LogicController instance;
+	
+	public static LogicController getInstance(){
+		
+		if(instance == null){
+			instance = new LogicController();
+		}
+		return instance;
+	}
+	
+	public String onCommandProcess(String command){
+		//Implement your logic here. Call parser etc etc.
+		
+		
+		return null;
+	}
+	
+	
+
+}
---
+++ src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UI.fxml"));
+			fxmlLoader.setController(new UIController());
+			Parent root = fxmlLoader.load();
+			Scene scene = new Scene(root, 640, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
