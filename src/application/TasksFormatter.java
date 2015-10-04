6ec9d184224ed9b30229b0893d63082cea66ae63
YouLiang Lim <a0125975@u.nus.edu>
2015-10-04 20:12:32 +0800

    added in support for displaying in PLAIN_VIEW_TYPE and DETAIL_VIEW_TYPE mode. added a new junit for Parser(yet to be implemented)

--- src/application/TasksFormatter.java
+++ src/application/TasksFormatter.java
@@ -12,14 +12,16 @@ public class TasksFormatter {
 	public static final int PRIORITY_VIEW_TYPE = 5;
 	public static final int PLACE_VIEW_TYPE = 6;
 	
-	private static final int DETAIL_DESCRIPTION_COUNT = 51;
-	private static final int DETAIL_START_DATE_COUNT = 18;
-	private static final int DETAIL_END_DATE_COUNT = 16;
+	private static final int DETAIL_DESCRIPTION_COUNT = 26;
+	private static final int DETAIL_START_DATE_COUNT = 17;
+	private static final int DETAIL_END_DATE_COUNT = 15;
 	private static final int DETAIL_LOCATION_COUNT = 16;
-	private static final int DETAIL_TYPE_COUNT = 12;
+	private static final int DETAIL_TYPE_COUNT = 10;
 	private static final int DETAIL_PRIORITY_COUNT = 16;
 	
-	private static final String DETAIL_VIEW_HEADER = "Description\t\t\t\t\tStart Date\tEnd Date\tLocation\tType\tPriority\n\n";
+	private static final String TAB_SPACE = "        ";
+	
+	private static final String DETAIL_VIEW_HEADER = "Description" + TAB_SPACE+ TAB_SPACE + TAB_SPACE+ "Start Date" + TAB_SPACE + "End Date" + TAB_SPACE + "Location" + TAB_SPACE + "Type" + TAB_SPACE + "Priority\n";
 	/**
 	 * @param lists
 	 *            : The lists of Task to be displayed
@@ -41,19 +43,30 @@ public class TasksFormatter {
 		case DETAIL_VIEW_TYPE:
 			
 			sb.append(DETAIL_VIEW_HEADER);
-			for(int c=0; c < DETAIL_VIEW_HEADER.length() + countNumOfTab(DETAIL_VIEW_HEADER); c++){
+			for(int c=0; c < DETAIL_VIEW_HEADER.length() - 10; c++){
 				sb.append("-");
 			}
 			sb.append("\n");
 			for(Task task: lists){
-				String s_date = DateFormat.getDateInstance().format(task.getStart_date());
-				String e_date =  DateFormat.getDateInstance().format(task.getEnd_date());
+				
+				String s_date = ""; 
+				String e_date = ""; 
+				
+				if(task.getStart_date() != null){
+					s_date = DateFormat.getDateInstance().format(task.getStart_date());
+				}
+				if(task.getEnd_date() != null){
+					e_date =  DateFormat.getDateInstance().format(task.getEnd_date());
+				}
+				
+				
 				appendWithSpaceOffset(sb, task.getTextContent(), DETAIL_DESCRIPTION_COUNT);
 				appendWithSpaceOffset(sb, s_date, DETAIL_START_DATE_COUNT);
 				appendWithSpaceOffset(sb, e_date, DETAIL_END_DATE_COUNT);
 				appendWithSpaceOffset(sb, task.getPlace_argument(), DETAIL_LOCATION_COUNT);
 				appendWithSpaceOffset(sb, task.getType_argument(),  DETAIL_TYPE_COUNT);
 				appendWithSpaceOffset(sb, task.getPriority_argument(),  DETAIL_PRIORITY_COUNT);
+				sb.append("\n");				
 			}
 			
 			break;
@@ -76,11 +89,12 @@ public class TasksFormatter {
 
 		}
 
-		return null; //stub
+		return sb.toString(); //stub
 	}
 	//PreCond : str must not be null
 	public static void appendWithSpaceOffset(StringBuilder sb, String str, int totalSpace) {
-		sb.append(str + getNoOfSpace(totalSpace - str.length()));
+		String n_str = replaceWithDotIfTooLong(str, totalSpace);
+		sb.append(n_str + getNoOfSpace(totalSpace - n_str.length()));
 	}
 	
 	private static String getNoOfSpace(int n){
@@ -90,17 +104,15 @@ public class TasksFormatter {
 		}
 		return sb.toString();
 	}
-
-	private static int countNumOfTab(String str){
-		int count = 0;
+	
+	private static String replaceWithDotIfTooLong(String string, int limit){
 		
-		for(int i=0; i<str.length(); i++){
-			if(str.charAt(i) == '\t'){
-				count++;
-			}	
-		}
-		return count;
-		
+		if(string.length() > (limit * 0.7)){
+			return string.substring(0, (int)(limit * 0.7)) + "..";
+			
+		}		
+		return string;
 	}
+
 	
 }
---
+++ test/application/ParserTest.java
@@ -0,0 +1,27 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class ParserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		ArrayList<Task> tasks = new ArrayList<Task>();
+		Task task1 = new Task("Buy Milk YOYOYOY");
+		task1.setEnd_date(DateFormat.getDateInstance().getCalendar().getTime());
+		task1.setStart_date(DateFormat.getDateInstance().getCalendar().getTime());
+		task1.setPriority_argument("high");
+		task1.setPlace_argument("bukit panjang");
+		task1.setType_argument("Meeting");
+		tasks.add(task1);
+		tasks.add(task1);
+		
+	}
+
+}
