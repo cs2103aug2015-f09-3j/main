7adacd4b4704075e5f504fab2c538682b8a41010
Lim Qi Wen <qiwen92@u.nus.edu>
2015-10-15 17:11:06 +0800

    Close #71, #51. Split DataManager into multiple classes.

--- src/application/DataManager.java
+++ src/application/DataManager.java
@@ -1,10 +1,8 @@
 package application;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Stack;
 
 import com.google.gson.*;
 
@@ -13,6 +11,8 @@ import com.google.gson.*;
  */
 
 public class DataManager {
+	
+	public static final Integer NO_PREV_COMMAND = -4;
 	public static final Integer TASK_ALREADY_EXISTS = -3;
 	public static final Integer MULTIPLE_MATCHES = -2;
 	public static final Integer TASK_NOT_FOUND = -1;
@@ -20,19 +20,14 @@ public class DataManager {
 	public static final Integer TASK_ADDED = 2;
 	public static final Integer TASK_SET_TO_DONE = 3;
 	public static final Integer TASK_UPDATED = 4;
+	public static final Integer PREV_COMMAND_UNDONE = 5;
+	public static final Integer MAX_HISTORY = 10;
 	
-	
-	private LocalStorage file;
-	private Gson gson;
-	private ArrayList<Task> taskList;
-	private ArrayList<Task> searchList;
+	private Data data;
 	public static DataManager instance = null;
 
 	private DataManager() {
-		file = new LocalStorage();
-		gson = new Gson();
-		taskList = initialiseTaskList();
-		searchList = new ArrayList<Task>();
+		data = new Data();
 	}
 
 	public static DataManager getInstance() {
@@ -43,18 +38,19 @@ public class DataManager {
 	}
 
 	public void addNewTask(Task taskToAdd) {
-		searchList.clear();
-		taskList.add(taskToAdd);
-		sort();
-		file.clear();
-		file.saveToFile(tasksToStrings());
+		data.clearSearchList();
+		if(taskToAdd.getPriority_argument().equals("")){
+			taskToAdd.setPriority_argument("Normal");
+		}
+		if(taskToAdd.getType_argument().equals("")){
+			taskToAdd.setType_argument("Normal");
+		}
+		data.addToData(taskToAdd);
 	}
 
 	public ArrayList<Task> listAll(Command cmd) {
-		searchList.clear();
-		DateFormat df1 = new SimpleDateFormat(Parser.DATE_FORMAT_TYPE_1);
 		if (cmd.getParameter().size() == 0){
-			return taskList;
+			return data.getTaskList();
 		}else{
 			ArrayList<Task> filteredList = new ArrayList<Task>();
 			ArrayList<Parameter> para = new ArrayList<Parameter>();
@@ -62,7 +58,7 @@ public class DataManager {
 			for(int i=0; i<para.size();i++){
 				switch(para.get(i).getParaType()){
 					case Parameter.PRIORITY_ARGUMENT_TYPE:
-						for(Task task: taskList){
+						for(Task task: data.getTaskList()){
 							if(task.getPriority_argument().equals(para.get(i).getParaArg())){
 								if(!filteredList.contains(task)){
 									filteredList.add(task);
@@ -71,7 +67,7 @@ public class DataManager {
 						}
 						break;
 					case Parameter.TYPE_ARGUMENT_TYPE:
-						for(Task task: taskList){
+						for(Task task: data.getTaskList()){
 							if(task.getType_argument().equals(para.get(i).getParaArg())){
 								if(!filteredList.contains(task)){
 									filteredList.add(task);
@@ -81,7 +77,7 @@ public class DataManager {
 						break;
 					case Parameter.START_DATE_ARGUMENT_TYPE:
 						try {
-							for(Task task: taskList){
+							for(Task task: data.getTaskList()){
 								if(task.getStart_date().equals(Parser.getInstance().parseDate(para.get(i).getParaArg()))){
 									if(!filteredList.contains(task)){
 										filteredList.add(task);
@@ -94,7 +90,7 @@ public class DataManager {
 							break;
 					case Parameter.END_DATE_ARGUMENT_TYPE:
 						try {
-							for(Task task: taskList){
+							for(Task task: data.getTaskList()){
 								if(task.getEnd_date().equals(Parser.getInstance().parseDate(para.get(i).getParaArg()))){
 									if(!filteredList.contains(task)){
 										filteredList.add(task);
@@ -106,7 +102,7 @@ public class DataManager {
 						}
 							break;
 					default:
-						for(Task task: taskList){
+						for(Task task: data.getTaskList()){
 							if(task.getPlace_argument().equals(para.get(i).getParaArg())){
 								if(!filteredList.contains(task)){
 									filteredList.add(task);
@@ -122,15 +118,14 @@ public class DataManager {
 	}
 
 	public Integer removeTask(Command cmd) {
-		searchList.clear();
-		searchTasksForMatches(cmd);
+		data.clearSearchList();
+		ArrayList<Task> searchList = searchTasksForMatches(cmd);
+		data.saveToSearchList(searchList);
 		switch (searchList.size()){
 			case 0:
 				return TASK_NOT_FOUND;
 			case 1:	
-				taskList.remove(searchList.get(0));
-				file.clear();
-				file.saveToFile(tasksToStrings());
+				data.removeFromData(searchList.get(0));
 				return TASK_REMOVED;
 			default:
 				LogicController.getInstance().chooseLine(tasksToStrings(searchList));
@@ -139,23 +134,21 @@ public class DataManager {
 	}
 	
 	public Integer removeTask(int lineNum){
-		taskList.remove(searchList.get(lineNum));
-		file.clear();
-		file.saveToFile(tasksToStrings(taskList));
+		data.removeFromData(data.getSearchList().get(lineNum));
 		return TASK_REMOVED;
 	}
 
 	public Integer editTask(Command cmd) {
-		searchList.clear();
-		searchTasksForMatches(cmd);
-
-		DateFormat df1 = new SimpleDateFormat(Parser.DATE_FORMAT_TYPE_1);
+		data.clearSearchList();
+		ArrayList<Task> searchList = searchTasksForMatches(cmd);
+		data.saveToSearchList(searchList);
 		switch (searchList.size()){
 			case 0:
 				return TASK_NOT_FOUND;
 			case 1:	
 				ArrayList<Parameter> para = new ArrayList<Parameter>();
 				para = cmd.getParameter();
+				ArrayList<Task> taskList = data.getTaskList();
 				for(int i=0; i<para.size(); i++){
 					switch(para.get(i).getParaType()){
 						case Parameter.PRIORITY_ARGUMENT_TYPE:
@@ -165,29 +158,19 @@ public class DataManager {
 							taskList.get(taskList.indexOf(searchList.get(0))).setType_argument(para.get(i).getParaArg());
 							break;
 						case Parameter.START_DATE_ARGUMENT_TYPE:
-						try {
-							taskList.get(taskList.indexOf(searchList.get(0))).setStart_date(df1.parse(para.get(i).getParaArg()));
-						} catch (ParseException e) {
-							// Do nothing, task remain the same.
-							e.printStackTrace();
-						}
+							taskList.get(taskList.indexOf(searchList.get(0))).setStart_date(
+									Parser.getInstance().parseDate(para.get(i).getParaArg()));
 							break;
 						case Parameter.END_DATE_ARGUMENT_TYPE:
-							try {
-								taskList.get(taskList.indexOf(searchList.get(0))).setEnd_date(df1.parse(para.get(i).getParaArg()));
-							} catch (ParseException e) {
-								// Do nothing, task remain the same.
-								e.printStackTrace();
-							}
+							taskList.get(taskList.indexOf(searchList.get(0))).setEnd_date(
+									Parser.getInstance().parseDate(para.get(i).getParaArg()));
 							break; 
 						default:
 							taskList.get(taskList.indexOf(searchList.get(0))).setPlace_argument(para.get(i).getParaArg());
 							break;
 					}
 				}
-				sort();
-				file.clear();
-				file.saveToFile(tasksToStrings());
+				data.updateStorage();
 				return TASK_UPDATED;
 			default:
 				LogicController.getInstance().chooseLine(tasksToStrings(searchList));
@@ -201,16 +184,17 @@ public class DataManager {
 	}
 
 	public Integer setDoneToTask(Command cmd){
-		searchList.clear();
-		searchTasksForMatches(cmd);
+		data.clearSearchList();
+		ArrayList<Task> searchList = searchTasksForMatches(cmd);
+		data.saveToSearchList(searchList);
+		ArrayList<Task> taskList = data.getTaskList();
 		switch (searchList.size()){
 			case 0:
 				return TASK_NOT_FOUND;
 			case 1:	
 				int index = taskList.indexOf(searchList.get(0));
 				taskList.get(index).setDone(true);
-				file.clear();
-				file.saveToFile(tasksToStrings(taskList));
+				data.updateStorage();
 				return TASK_SET_TO_DONE;
 			default:
 				LogicController.getInstance().chooseLine(tasksToStrings(searchList));
@@ -219,13 +203,122 @@ public class DataManager {
 	}
 	
 	public Integer setDoneToTask(int lineNum){
-		int index = taskList.indexOf(searchList.get(lineNum));
-		taskList.get(index).setDone(true);
+		int index = data.getTaskList().indexOf(data.getSearchList().get(lineNum));
+		data.getTaskList().get(index).setDone(true);
+		data.updateStorage();
 		return TASK_SET_TO_DONE;
 	}
 
 	public Integer changeStorageLocation(Command cmd) {
-		return file.changePath(cmd.getTextContent());
+		return data.changeFileLocation(cmd.getTextContent());
+	}
+	
+	public Integer undoPrevCommand(){
+		return data.undo();
+	}
+	
+	private ArrayList<String> tasksToStrings(ArrayList<Task> list){
+		ArrayList<String> taskStrings = new ArrayList<String>();
+		for(int i=0;i<list.size();i++){
+			taskStrings.add(list.get(i).getTextContent());
+		}
+		return taskStrings;
+	}
+
+	private ArrayList<Task> searchTasksForMatches(Command cmd) {
+		ArrayList<Task> searchList = new ArrayList<Task>();
+		for(int i=0; i<data.getTaskList().size();i++){
+			if(data.getTaskList().get(i).getTextContent().contains(cmd.getTextContent())){
+				searchList.add(data.getTaskList().get(i));
+			}
+		}
+		return searchList;
+	}
+	
+	private void sort(ArrayList<Task> list){
+		Collections.sort(list);
+	}
+}
+
+class Data{
+	private ArrayList<Task> taskList;
+	private ArrayList<Task> searchList;
+	private Stack<ArrayList<Task>> history;
+	private StorageInterface storageIO;
+	private Gson gson;
+	private int histCount;
+	
+	public Data(){
+		storageIO = new StorageInterface();
+		gson = new Gson();
+		taskList = initializeTaskList();
+		searchList = new ArrayList<Task>();
+		history = new Stack<ArrayList<Task>>();
+		histCount = 0;
+	}
+	
+	
+	public ArrayList<Task> getTaskList(){
+		return taskList;
+	}
+	public ArrayList<Task> getSearchList(){
+		return searchList;
+	}
+	
+	public void saveToSearchList(ArrayList<Task> list){
+		searchList = list;
+	}
+	public void clearSearchList(){
+		searchList.clear();
+	}
+	
+	public void addToData(Task taskToAdd){
+		taskList.add(taskToAdd);
+		updateStorage();
+	}
+	public void updateStorage(){
+		sort();
+		history.push(taskList);
+		histCount ++;
+		limitHistory();
+		storageIO.saveToStorage(tasksToStrings());
+	}
+	public void removeFromData(Task taskToRemove){
+		taskList.remove(taskToRemove);
+		storageIO.saveToStorage(tasksToStrings());
+	}
+	public void removeFromData(int lineNum){
+		taskList.remove(lineNum);
+		storageIO.saveToStorage(tasksToStrings());
+	}
+	public Integer undo(){
+		if(history.empty()){
+			return DataManager.NO_PREV_COMMAND;
+		}else{
+			taskList = history.pop();
+			histCount --;
+			storageIO.saveToStorage(tasksToStrings());
+			return DataManager.PREV_COMMAND_UNDONE;
+		}
+	}
+	
+	public Integer changeFileLocation(String location){
+		return storageIO.changeFilePath(location);
+	}
+	
+	private ArrayList<Task> initializeTaskList(){
+		ArrayList<String> listString = storageIO.readFromStorage();
+		ArrayList<Task> listTask = new ArrayList<Task>();
+		
+		for(int i=0; i<listString.size(); i++){
+			listTask.add(gson.fromJson(listString.get(i),Task.class));
+		}
+		
+		return listTask;
+	}
+	
+	private void sort(){
+		Collections.sort(taskList);
 	}
 	
 	private ArrayList<String> tasksToStrings(){
@@ -236,37 +329,40 @@ public class DataManager {
 		return taskStrings;
 	}
 	
-	private ArrayList<String> tasksToStrings(ArrayList<Task> list){
-		ArrayList<String> taskStrings = new ArrayList<String>();
-		for(int i=0;i<list.size();i++){
-			taskStrings.add(list.get(i).getTextContent());
-		}
-		return taskStrings;
-	}
-	
-	private void sort(){
-		Collections.sort(taskList);
-	}
-	
-	private void sort(ArrayList<Task> list){
-		Collections.sort(list);
-	}
-	
-	private ArrayList<Task> initialiseTaskList(){
-		ArrayList<Task> list = new ArrayList<Task>();
-		ArrayList<String> stringList = file.readFile();
-		for(int i=0; i<stringList.size();i++){
-			list.add(gson.fromJson(stringList.get(i), Task.class));
-		}
-		return list;
-	}
-
-	private void searchTasksForMatches(Command cmd) {
-		for(int i=0; i<taskList.size();i++){
-			if(taskList.get(i).getTextContent().contains(cmd.getTextContent())){
-				searchList.add(taskList.get(i));
+	private void limitHistory(){
+		Stack<ArrayList<Task>> tempStack = new Stack<ArrayList<Task>>();
+		if(histCount > 10){	
+			while(!history.empty()){
+				tempStack.push(history.pop());
 			}
+			tempStack.pop();
+			while(!tempStack.empty()){
+				history.push(tempStack.pop());
+			}
+			histCount --;
 		}
 	}
 }
 
+class StorageInterface{
+	private LocalStorage file;
+	
+	public StorageInterface(){
+		file = new LocalStorage();
+	}
+	
+	public ArrayList<String> readFromStorage(){
+		return file.readFile();
+	}
+	
+	public void saveToStorage(ArrayList<String> list){
+		file.clear();
+		file.saveToFile(list);
+	}
+	
+	public Integer changeFilePath(String path){
+		return file.changePath(path);
+	}
+	
+	
+}
