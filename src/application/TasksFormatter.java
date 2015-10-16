2e59ae9bbdb190fe21f80c06ed9336b2c7fe4077
nghuiyirebecca <nghuiyirebecca@hotmail.com>
2015-10-16 12:31:34 +0800

    remove false assertion in taskformatter

--- src/application/TasksFormatter.java
+++ src/application/TasksFormatter.java
@@ -14,21 +14,21 @@ public class TasksFormatter {
 	public static final int TYPE_VIEW_TYPE = 4;
 	public static final int PRIORITY_VIEW_TYPE = 5;
 	public static final int PLACE_VIEW_TYPE = 6;
-	
+
 	private static final int DETAIL_DESCRIPTION_COUNT = 35;
 	private static final int DETAIL_START_DATE_COUNT = 25;
 	private static final int DETAIL_END_DATE_COUNT = 25;
 	private static final int DETAIL_LOCATION_COUNT = 25;
 	private static final int DETAIL_TYPE_COUNT = 15;
 	private static final int DETAIL_PRIORITY_COUNT = 20;
-	
+
 
 	private static final String OUTPUT_FORMAT_HEADER = "%-30s %-20s %-20s %-20s %-15s %-15s";
 	private static final String OUTPUT_FORMAT = "%-3d %-30s %-20s %-20s %-20s %-15s %-15s";
 
 
 	private static final String DETAIL_VIEW_HEADER = "    " + String.format(OUTPUT_FORMAT_HEADER, "Description", "Start Date", "End Date", "Location", "Type", "Priority");
-	
+
 	/**
 	 * @param lists
 	 *            : The lists of Task to be displayed
@@ -37,7 +37,6 @@ public class TasksFormatter {
 	 * @return the formatted text in String format.
 	 */
 	public static String format(ArrayList<Task> lists, int typeOfFormatting) {
-		assert false;
 		StringBuilder sb = new StringBuilder();
 		DateFormat df1 = new SimpleDateFormat(Parser.DATE_FORMAT_TYPE_101);
 		switch (typeOfFormatting) {
@@ -50,16 +49,16 @@ public class TasksFormatter {
 			break;
 
 		case DETAIL_VIEW_TYPE:
-			
+
 			sb.append(DETAIL_VIEW_HEADER);
 
 			sb.append("\n");
 			int count = 1;
-			for(Task task: lists){ 
-				
-				String s_date = ""; 
-				String e_date = ""; 
-				
+			for(Task task: lists){
+
+				String s_date = "";
+				String e_date = "";
+
 				if(task.getStart_date() != null){
 					s_date = df1.format(task.getStart_date());
 					//s_date = DateFormat.getDateInstance().format(task.getStart_date());
@@ -70,11 +69,11 @@ public class TasksFormatter {
 				}
 				sb.append(String.format(OUTPUT_FORMAT, count++, replaceWithDotIfTooLong(task.getTextContent(),DETAIL_DESCRIPTION_COUNT), s_date, e_date, replaceWithDotIfTooLong(task.getPlace_argument(),DETAIL_LOCATION_COUNT)
 						,replaceWithDotIfTooLong(task.getType_argument(),DETAIL_TYPE_COUNT),replaceWithDotIfTooLong(task.getPriority_argument(),DETAIL_PRIORITY_COUNT)));
-				
 
-				sb.append("\n");				
+
+				sb.append("\n");
 			}
-			
+
 			break;
 
 		case TIMELINE_VIEW_TYPE:
@@ -97,18 +96,18 @@ public class TasksFormatter {
 
 		return sb.toString(); //stub
 	}
-	
 
 
-	
+
+
 	private static String replaceWithDotIfTooLong(String string, int limit){
-		
+
 		if(string.length() > (limit * 0.7)){
 			return string.substring(0, (int)(limit * 0.7)) + "..";
-			
-		}		
+
+		}
 		return string;
 	}
 
-	
+
 }
