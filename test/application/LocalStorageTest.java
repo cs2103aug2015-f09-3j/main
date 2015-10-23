545357ffab587683dea1e96fb3d357f8e042c3ab
Zhang Lei <akatsuki421@gmail.com>
2015-10-23 09:36:14 +0800

    update Test folder

--- test/application/LocalStorageTest.java
+++ test/application/LocalStorageTest.java
@@ -1,8 +1,10 @@
-package application.model;
+package application;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import application.model.LocalStorage;
 
 public class LocalStorageTest {
 
