0249e7aa80c9c52a966dd1dca0d82ef14411726c
You Liang <a0125975@u.nus.edu>
2015-11-02 15:15:30 +0800

    first commit for new branch. still trying.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,5 @@
 package application;
+
 //@@LimYouLiang A0125975U
 import java.awt.AWTException;
 import java.awt.Image;
@@ -8,10 +9,31 @@ import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
 import com.melloware.jintellitype.JIntellitype;
 
 import application.view.UIController;
@@ -22,115 +44,194 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application { 
-	
+public class Main extends Application {
+
 	boolean firstTime;
-	 private TrayIcon trayIcon;
-	 public Stage stage;
+	private TrayIcon trayIcon;
+	public Stage stage;
+
+	/** Application name. */
+	private static final String APPLICATION_NAME = "toDoo";
+
+	/** Directory to store user credentials for this application. */
+	private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
+			".credentials/toDoo");
+
+	/** Global instance of the {@link FileDataStoreFactory}. */
+	private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+	/** Global instance of the JSON factory. */
+	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+	/** Global instance of the HTTP transport. */
+	private static HttpTransport HTTP_TRANSPORT;
+
+	/** Global instance of the scopes required by this quickstart. */
+	private static final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR_READONLY);
+
+	static {
+		try {
+			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+		} catch (Throwable t) {
+			t.printStackTrace();
+			System.exit(1);
+		}
+	}
 	
+	
+
+	/**
+	 * Creates an authorized Credential object.
+	 * 
+	 * @return an authorized Credential object.
+	 * @throws IOException
+	 */
+	public Credential authorize() throws IOException {
+		// Load client secrets.
+		InputStream in = this.getClass().getResourceAsStream("client_secret.json");
+		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+		// Build flow and trigger user authorization request.
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
+		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+		System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+		return credential; 
+	}
+
+	/**
+	 * Build and return an authorized Calendar client service.
+	 * 
+	 * @return an authorized Calendar client service
+	 * @throws IOException
+	 */
+	public com.google.api.services.calendar.Calendar getCalendarService() throws IOException {
+		Credential credential = authorize();
+		return new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+				.setApplicationName(APPLICATION_NAME).build();
+	}
+
 	@Override
 	public void start(Stage primaryStage) {
+		
 		try {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UI.fxml"));
 			UIController uiControl = UIController.getInstance();
-			
+
 			fxmlLoader.setController(uiControl);
-			
+
 			Parent root = fxmlLoader.load();
 			Scene scene = new Scene(root, 1007, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("toDoo");
-			primaryStage.getIcons().add( new javafx.scene.image.Image(
-					getClass().getResourceAsStream( "toDoo.png" )));  
+			primaryStage.getIcons().add(new javafx.scene.image.Image(getClass().getResourceAsStream("toDoo.png")));
 			uiControl.setMainApp(this);
 			firstTime = true;
-		    Platform.setImplicitExit(false);  
+			Platform.setImplicitExit(false);
 			primaryStage.show();
-			
+
 			createAndShowGUI();
 			this.stage = primaryStage;
-			
-		
-			
-			
-		} catch (Exception e) { 
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		try {
+			com.google.api.services.calendar.Calendar service = getCalendarService();
+
+			// List the next 10 events from the primary calendar.
+			DateTime now = new DateTime(System.currentTimeMillis());
+			Events events = service.events().list("primary").setMaxResults(10).setTimeMin(now).setOrderBy("startTime")
+					.setSingleEvents(true).execute();
+			List<Event> items = events.getItems();
+			if (items.size() == 0) {
+				System.out.println("No upcoming events found.");
+			} else {
+				System.out.println("Upcoming events");
+				for (Event event : items) {
+					DateTime start = event.getStart().getDateTime();
+					if (start == null) {
+						start = event.getStart().getDate();
+					}
+					System.out.printf("%s (%s)\n", event.getSummary(), start);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void main(String[] args) {
-		
+
 		launch(args);
+
 	}
-	
+
 	/**
-	 * @@Java
-	 * DISCLAIMER: this code is adapted from java official code examples.
+	 * @@Java DISCLAIMER: this code is adapted from java official code examples.
 	 */
-	 private void createAndShowGUI() {
-	        //Check the SystemTray support
-	        if (!SystemTray.isSupported()) { 
-	            System.out.println("SystemTray is not supported");
-	            return;
-	        } 
-	        final PopupMenu popup = new PopupMenu();    
-	        trayIcon =
-	                new TrayIcon(createImage("toDoo.png", "tray icon"));
-	        final SystemTray tray = SystemTray.getSystemTray();  
-	         
+	private void createAndShowGUI() {
+		// Check the SystemTray support
+		if (!SystemTray.isSupported()) {
+			System.out.println("SystemTray is not supported");
+			return;
+		}
+		final PopupMenu popup = new PopupMenu();
+		trayIcon = new TrayIcon(createImage("toDoo.png", "tray icon"));
+		final SystemTray tray = SystemTray.getSystemTray();
 
-	        MenuItem exitItem = new MenuItem("Exit");
+		MenuItem exitItem = new MenuItem("Exit");
 
-	        popup.add(exitItem);
-	        
-	        trayIcon.setPopupMenu(popup);
-	        
-	        try {
-	            tray.add(trayIcon);
-	        } catch (AWTException e) {
-	            System.out.println("TrayIcon could not be added.");
-	            return;
-	        }
-	      
-	        
-	        trayIcon.addActionListener(new ActionListener() { 
-	            @Override
-				public void actionPerformed(ActionEvent e) {
-	            	 Platform.runLater(new Runnable() {   
-	                        @Override
-	                        public void run() { 
-	                            stage.show();
-	                        }
-	                    });
-	            	
-	            }
-	        });
-	        
-	        exitItem.addActionListener(new ActionListener() {
-	            @Override
-				public void actionPerformed(ActionEvent e) {
-	                tray.remove(trayIcon);
-	                JIntellitype.getInstance().cleanUp();
-	                System.exit(0);
-	            }
-	        });
-	    } 
-	    
-	    //Obtain the image URL
-	    protected static Image createImage(String path, String description) {  
-	    	
-	        URL imageURL = Main.class.getResource(path); 
-	        
-	        if (imageURL == null) {  
-	            System.err.println("Resource not found: " + path);
-	            return null;  
-	        } else {
-	            return (new ImageIcon(imageURL, description)).getImage();
-	        }
-	    }
+		popup.add(exitItem);
 
+		trayIcon.setPopupMenu(popup);
 
-	   
-	
+		try {
+			tray.add(trayIcon);
+		} catch (AWTException e) {
+			System.out.println("TrayIcon could not be added.");
+			return;
+		}
+
+		trayIcon.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						stage.show();
+					}
+				});
+
+			}
+		});
+
+		exitItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tray.remove(trayIcon);
+				JIntellitype.getInstance().cleanUp();
+				System.exit(0);
+			}
+		});
+	}
+
+	// Obtain the image URL
+	protected static Image createImage(String path, String description) {
+
+		URL imageURL = Main.class.getResource(path);
+
+		if (imageURL == null) {
+			System.err.println("Resource not found: " + path);
+			return null;
+		} else {
+			return (new ImageIcon(imageURL, description)).getImage();
+		}
+	}
+
 }
