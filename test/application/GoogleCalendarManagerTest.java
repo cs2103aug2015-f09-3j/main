c3fd80bcc055686e35bf922ddb1e8cce956a6947
You Liang <a0125975@u.nus.edu>
2015-11-05 17:54:03 +0800

    fix the freezing issue

---
+++ test/application/GoogleCalendarManagerTest.java
@@ -0,0 +1,20 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GoogleCalendarManagerTest {
+
+	@Test
+	public void testQuickAdd() {
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
